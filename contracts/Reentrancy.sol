cd67b3ed4c17d0bc527f917866e420a1361355c1
Younes <younesgt350@gmail.com>
2023-10-02 09:37:33 +0200

    finished reentrancy

--- contracts/Reentrancy.sol
+++ contracts/Reentrancy.sol
@@ -14,10 +14,10 @@ contract EtherStore {
 
     function withdraw() external {
         uint256 balance = balances[msg.sender];
-        require(balance > 0);
+        require(balance > 0, "Not enough balance");
+        balances[msg.sender] = 0;
         (bool success, ) = msg.sender.call{value: balance}("");
         require(success, "Failed to send Ether");
-        balances[msg.sender] = 0;
     }
 
     // Helper function to check the balance of this contract
